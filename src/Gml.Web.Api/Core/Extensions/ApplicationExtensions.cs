8425c94172f02144ad54ffbcf73c0e81c31ba1a2
Anton Terentev <gamervii@icloud.com>
2024-02-11 18:48:37 +0300

    Refactor service registration in WebApplicationBuilder
    The ApplicationExtensions class now fetches server settings directly from the builder configuration. Each part of service registration (options, endpoints info, system components) is split into separate methods, enhancing readability and project rights management with better policy handling.

--- src/Gml.Web.Api/Core/Extensions/ApplicationExtensions.cs
+++ src/Gml.Web.Api/Core/Extensions/ApplicationExtensions.cs
@@ -20,46 +20,61 @@ public static class ApplicationExtensions
 
     public static WebApplicationBuilder RegisterServices(this WebApplicationBuilder builder)
     {
-        var configuration = ConfigurationSection(
-            builder,
+        var serverSettings = GetServerSettings(builder,
             out var projectName,
             out var projectDescription,
             out var policyName);
 
-        // Options
+        builder.RegisterOptions(serverSettings);
+        builder.RegisterEndpointsInfo(projectName, projectDescription);
+        builder.RegisterSystemComponents(policyName);
+
+        return builder;
+    }
+
+    private static IConfigurationSection GetServerSettings(WebApplicationBuilder builder, out string projectName,
+        out string? projectDescription, out string policyName)
+    {
+        var serverConfiguration = builder.Configuration.GetSection(nameof(ServerSettings));
+
+        projectName = serverConfiguration.GetValue<string>("ProjectName") ??
+                      throw new Exception("Project name not found");
+        projectDescription = serverConfiguration.GetValue<string>("ProjectDescription");
+        policyName = serverConfiguration.GetValue<string>("PolicyName") ?? throw new Exception("Policy name not found");
+
+        return serverConfiguration;
+    }
+
+    public static WebApplicationBuilder RegisterOptions(this WebApplicationBuilder builder,
+        IConfigurationSection serverSettings)
+    {
         builder.Services
             .AddOptions<ServerSettings>()
-            .Bind(configuration);
+            .Bind(serverSettings);
 
+        return builder;
+    }
+
+    private static WebApplicationBuilder RegisterEndpointsInfo(this WebApplicationBuilder builder, string projectName,
+        string? projectDescription)
+    {
         builder.Services
-            // Register endpoints info
             .AddEndpointsApiExplorer()
-            .RegisterSwagger(projectName, projectDescription)
+            .RegisterSwagger(projectName, projectDescription);
 
-            // Add system components
+        return builder;
+    }
+
+    private static WebApplicationBuilder RegisterSystemComponents(this WebApplicationBuilder builder, string policyName)
+    {
+        builder.Services
             .AddDbContext<DatabaseContext>(options =>
                 options.UseSqlite(builder.Configuration.GetConnectionString("SQLite")))
             .AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies().AsEnumerable())
             .RegisterRepositories()
             .RegisterValidators()
             .RegisterCors(policyName);
+
         return builder;
-    }
-
-    private static IConfigurationSection ConfigurationSection(WebApplicationBuilder builder, out string projectName,
-        out string? projectDescription, out string policyName)
-    {
-        var configuration = builder.Configuration;
-        var serverConfiguration = configuration.GetSection(nameof(ServerSettings));
-
-        projectName = serverConfiguration.GetValue<string>("ProjectName")
-                      ?? throw new Exception("Project name not found");
-
-        projectDescription = serverConfiguration.GetValue<string>("ProjectDescription");
-
-        policyName = serverConfiguration.GetValue<string>("PolicyName")
-                     ?? throw new Exception("Policy name not found");
-        
-        return serverConfiguration;
     }
 }
