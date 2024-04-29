fb1b97c5bdcb8ea1f5d5b5171f8a6a154061cbe2
Anton Terentev <gamervii@icloud.com>
2024-04-29 15:54:54 +0300

    Add Settings table to the database
    The commit introduces a new 'Settings' table to the database along with its migration files. The table is created with two columns, 'Id' and 'RegistrationIsEnabled'. The 'Down' migration has also been included for dropping the table if necessary.

---
+++ src/Gml.Web.Api/Migrations/20240429123721_Add settings table.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gml.Web.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addsettingstable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RegistrationIsEnabled = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Settings");
+        }
+    }
+}
