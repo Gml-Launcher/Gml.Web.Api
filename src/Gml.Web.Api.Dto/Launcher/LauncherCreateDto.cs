f45233414ab77b143be00f2e458384e21aa2ed17
Anton Terentev <gamervii@icloud.com>
2024-05-02 11:08:24 +0300

    Add LauncherCreateDto and its validator
    Implemented a new DTO class, LauncherCreateDto, with related properties for the creation of a launcher. Also, added a FluentValidation validation rules for LauncherCreateDto ensuring fields are not empty, have correct length, and Folder doesn't contain invalid characters.

---
+++ src/Gml.Web.Api.Dto/Launcher/LauncherCreateDto.cs
@@ -0,0 +1,8 @@
+namespace Gml.Web.Api.Dto.Launcher;
+
+public class LauncherCreateDto
+{
+    public string GitHubVersions { get; set; }
+    public string Host { get; set; }
+    public string Folder { get; set; }
+}
