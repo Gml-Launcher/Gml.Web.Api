e5fb75bb6a13ec8349bb0c3d260b0790bf8deded
Anton Terentev <gamervii@icloud.com>
2024-05-05 18:19:52 +0300

    Add storage settings and MinIO service in Docker
    New settings for storage have been added to the Gml.Web.Api migrations. The MinIO service has been included in the docker-compose and docker-compose-prod files. Changes also reflected in the .env.example file.

---
+++ src/Gml.Web.Api/Migrations/20240504122646_UpdateSettings.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gml.Web.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StorageHost",
+                table: "Settings",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StorageLogin",
+                table: "Settings",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StoragePassword",
+                table: "Settings",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StorageType",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StorageHost",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "StorageLogin",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "StoragePassword",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "StorageType",
+                table: "Settings");
+        }
+    }
+}
