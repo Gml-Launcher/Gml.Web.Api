7fa4a015febce08f7b2499dd770fccfabf7c4296
Anton Terentev <gamervii@icloud.com>
2024-05-11 12:11:55 +0300

    Add Avanguard plugin to Gml solution
    A new Avanguard plugin has been added to the Gml.Web.Api solution. The changes include the addition of the Gml.Web.Api.Plugin.Avanguard.csproj and GravitGuardEndpoint.cs files. References in the solution and project files have also been updated accordingly.

---
+++ Gml.Web.Api.Plugin.Avanguard/GravitGuardEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Gml.Web.Api.EndpointSDK;
+using Gml.Web.Api.Plugin.Avanguard.Core;
+using GmlCore.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Gml.Web.Api.Plugin.Avanguard;
+
+[Path("post", "/api/v1/plugins/avanguard/compile", true)]
+public class GravitGuardEndpoint : IPluginEndpoint
+{
+    public async Task Execute(HttpContext context, IGmlManager gmlManager)
+    {
+        
+        
+        await context.Response.WriteAsync("templaацуфацфуаte");
+    }
+}
