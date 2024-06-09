117d586939dbd549034a570f5725650a9466f6d3
Anton Terentev <gamervii@icloud.com>
2024-06-09 07:11:43 +0300

    Update ConfigureWebHost in GmlApiApplicationFactory
    The update comprises of a new in-memory configuration dictionary in ConfigureWebHost method of GmlApiApplicationFactory class in the test project. This update refines the services.AddAuthentication code block and introduces a ConfigureAppConfiguration block to add specific configurations. The configuration values are set in an in-memory collection, improving the way settings are managed during testing.

--- tests/Gml.WebApi.Tests/GmlApiApplicationFactory.cs
+++ tests/Gml.WebApi.Tests/GmlApiApplicationFactory.cs
@@ -2,7 +2,9 @@ using Gml.Web.Api.Core.Middlewares;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;  // For configuration builder
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;  // For dictionary
 
 namespace Gml.WebApi.Tests;
 
@@ -11,17 +13,28 @@ internal class GmlApiApplicationFactory : WebApplicationFactory<Program>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
-        {
-            //
-            // services.AddAuthentication("TestScheme")
-            //     .AddScheme<AuthenticationSchemeOptions, TestAuthenticationHandler>("TestScheme", options => { });
-
-            services.AddAuthentication(options =>
             {
-                options.DefaultAuthenticateScheme = "TestScheme";
-                options.DefaultScheme = "TestScheme";
-                options.DefaultChallengeScheme = "TestScheme";
-            }).AddScheme<AuthenticationSchemeOptions, TestAuthenticationHandler>("TestScheme", options => { });
-        });
+                services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = "TestScheme";
+                    options.DefaultScheme = "TestScheme";
+                    options.DefaultChallengeScheme = "TestScheme";
+                }).AddScheme<AuthenticationSchemeOptions, TestAuthenticationHandler>("TestScheme", options => { });
+            })
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                var configuration = new ConfigurationBuilder()
+                    .AddInMemoryCollection(new Dictionary<string, string>
+                    {
+                        {"SECURITY_KEY", "jkuhbsfgvuk4gfikhn8i7wa34rkbqw23"},
+                        {"PROJECT_NAME", "GmlServer"},
+                        {"PROJECT_DESCRIPTION", "GmlServer Description"},
+                        {"PROJECT_POLICYNAME", "GmlPolicy"},
+                        {"PROJECT_PATH", ""}
+                    }!)
+                    .Build();
+
+                config.AddConfiguration(configuration);
+            });
     }
 }
