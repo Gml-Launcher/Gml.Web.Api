4e961dfb4b6e944d66a120612871a15c2b62a0e4
akemiko <tcop29711@gmail.com>
2024-08-08 22:00:43 +0300

    Enhance SentryHandler with additional bug tracking details.
    Added new fields for capturing detailed memory info, user details, and exception stack traces in bug reports. This extends the scope of captured data to provide more comprehensive diagnostics.

--- src/Gml.Web.Api/Core/Handlers/SentryHandler.cs
+++ src/Gml.Web.Api/Core/Handlers/SentryHandler.cs
@@ -3,6 +3,7 @@ using Gml.Web.Api.Core.Services;
 using Gml.Web.Api.Dto.Sentry;
 using GmlCore.Interfaces;
 using Newtonsoft.Json;
+using MemoryInfo = Gml.Core.Launcher.MemoryInfo;
 
 namespace Gml.Web.Api.Core.Handlers;
 
@@ -35,7 +36,45 @@ public abstract class SentryHandler : ISentryHandler
 
         gmlManager.BugTracker.CaptureException(new BugInfo
         {
+            PcName = sentryModules.ServerName,
+            Username = sentryModules.User.Username,
+            MemoryInfo = new MemoryInfo
+            {
+                AllocatedBytes = sentryModules.Contexts.MemoryInfo.AllocatedBytes,
+                HighMemoryLoadThresholdBytes = sentryModules.Contexts.MemoryInfo.HighMemoryLoadThresholdBytes,
+                TotalAvailableMemoryBytes = sentryModules.Contexts.MemoryInfo.TotalAvailableMemoryBytes,
+                Compacted = sentryModules.Contexts.MemoryInfo.Compacted,
+                Concurrent = sentryModules.Contexts.MemoryInfo.Concurrent,
+                PauseDurations = sentryModules.Contexts.MemoryInfo.PauseDurations,
+            },
+            Exceptions = sentryModules.Exception.Values.Select(x => new ExceptionReport
+            {
+                Type = x.Type,
+                ValueData = x.ValueData,
+                Module = x.Module,
+                ThreadId = x.ThreadId,
+                Id = x.Id,
+                Crashed = x.Crashed,
+                Current = x.Current,
+                StackTrace = x.Stacktrace.Frames.Select(x => new StackTrace
+                {
+                    Filename = x.Filename,
+                    Function = x.Function,
+                    Lineno = x.Lineno,
+                    Colno = x.Colno,
+                    AbsPath = x.AbsPath,
+                    InApp = x.InApp,
+                    Package = x.Package,
+                    InstructionAddr = x.InstructionAddr,
+                    AddrMode = x.AddrMode,
+                    FunctionId = x.FunctionId
+                }),
 
+            }),
+            SendAt = sentryEvent.SentAt,
+            IpAddress = sentryModules.User.IpAddress,
+            OsVeriosn = sentryModules.Contexts.Os.RawDescription,
+            OsIdentifier = sentryModules.Contexts.Runtime.Type
         });
 
         return Results.Empty;
