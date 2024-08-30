a154b9ac3d81b638f0e27f9909ddd5256d5ed0dd
akemiko <tcop29711@gmail.com>
2024-08-30 20:57:17 +0300

    Rename FolderReadDto to ProfileFolderReadDto
    This change updates the class and related references to better reflect their purpose within the profile context. The modification ensures consistency in class naming conventions, making the codebase more maintainable and easier to understand.

--- src/Gml.Web.Api/Core/MappingProfiles/SystemIOMapper.cs
+++ src/Gml.Web.Api/Core/MappingProfiles/SystemIOMapper.cs
@@ -9,7 +9,7 @@ public class SystemIOMapper : Profile
     public SystemIOMapper()
     {
         CreateMap<LocalFileInfo, ProfileFileReadDto>();
-        CreateMap<FolderWhiteListDto, FolderReadDto>();
+        CreateMap<FolderWhiteListDto, ProfileFolderReadDto>();
         CreateMap<FolderWhiteListDto, LocalFolderInfo>();
     }
 }
