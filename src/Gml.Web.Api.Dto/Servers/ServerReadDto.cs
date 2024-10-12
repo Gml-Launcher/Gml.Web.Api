9fe882759edb6b17bbdfb6222f56726850ff6165
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-10-12 21:47:58 +0300

    Remove Address and Port properties from ServerReadDto
    This commit refines the ServerReadDto model by removing the Address and Port properties. This change simplifies the data transfer object and aligns better with current application requirements.

--- src/Gml.Web.Api.Dto/Servers/ServerReadDto.cs
+++ src/Gml.Web.Api.Dto/Servers/ServerReadDto.cs
@@ -3,8 +3,6 @@ namespace Gml.Web.Api.Dto.Servers;
 public class ServerReadDto
 {
     public string Name { get; set; }
-    public string Address { get; set; }
-    public int Port { get; set; }
     public string Version { get; set; }
     public bool IsOnline { get; set; }
     public int? Online { get; set; }
