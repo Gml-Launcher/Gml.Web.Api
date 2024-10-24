f977be2c32f57f28a2455a458af1bfb5225ebbf0
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-10-24 06:13:01 +0300

    Add TextureProtocol column to Settings table
    Introduced a new migration that adds a TextureProtocol column to the Settings table. Updated the DatabaseContextFactory to support SQLite connections and modified appsettings.json with the SQLite connection string.

--- src/Gml.Web.Api/Migrations/20241024031132_AddTextureProtocolColumn.Designer.cs
+++ src/Gml.Web.Api/Migrations/20241024031132_AddTextureProtocolColumn.Designer.cs
@@ -2,6 +2,7 @@
 using Gml.Web.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Gml.Web.Api.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20241024031132_AddTextureProtocolColumn")]
+    partial class AddTextureProtocolColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
---
+++ src/Gml.Web.Api/Migrations/20241024031132_AddTextureProtocolColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gml.Web.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTextureProtocolColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TextureProtocol",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TextureProtocol",
+                table: "Settings");
+        }
+    }
+}
