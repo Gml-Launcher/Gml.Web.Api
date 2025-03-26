5e208f7f1a3794c0672d76ab543679757040b651
Terentev A. A. <terentev.a.a@recloud.tech>
2025-03-26 18:05:14 +0300

    Fix missing access modifier for NewsListenerType property
    The `Type` property in `NewsReadDto` was inaccessible due to the missing `public` modifier. This change ensures the property is exposed correctly for external usage.

--- src/Gml.Web.Api.Dto/News/NewsReadDto.cs
+++ src/Gml.Web.Api.Dto/News/NewsReadDto.cs
@@ -8,5 +8,5 @@ public record NewsReadDto
     public string Title { get; set; }
     public string Content { get; set; }
     public DateTimeOffset Date { get; set; }
-    NewsListenerType Type { get; set; }
+    public NewsListenerType Type { get; set; }
 }
