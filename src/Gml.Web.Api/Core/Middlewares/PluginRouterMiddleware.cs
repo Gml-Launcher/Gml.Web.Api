0a7f3073978228d13a33df3268981266973aca33
Terentev A. A. <terentev.a.a@recloud.tech>
2025-07-01 14:11:30 +0300

    Add authentication check in `PluginRouterMiddleware` for secured endpoints
    - Added logic to enforce authentication if `PathAttribute` on an endpoint specifies `NeedAuth: true`.
    - Respond with 401 status code for unauthenticated requests to secured endpoints.

--- src/Gml.Web.Api/Core/Middlewares/PluginRouterMiddleware.cs
+++ src/Gml.Web.Api/Core/Middlewares/PluginRouterMiddleware.cs
@@ -1,4 +1,6 @@
-﻿using Gml.Web.Api.Core.Services;
+﻿using System.Reflection;
+using Gml.Web.Api.Core.Services;
+using Gml.Web.Api.EndpointSDK;
 using GmlCore.Interfaces;
 
 namespace Gml.Web.Api.Core.Middlewares;
@@ -9,6 +11,15 @@ public class PluginRouterMiddleware(RequestDelegate next)
     {
         if (PluginRouter.TryGetEndpoint(context.Request.Method, context.Request.Path, out var endpoint))
         {
+            if (endpoint.GetType().GetCustomAttributes(typeof(PathAttribute)).FirstOrDefault() is PathAttribute
+                {
+                    NeedAuth: true
+                } && context.User.Identity?.IsAuthenticated == false)
+            {
+                context.Response.StatusCode = 401;
+                return;
+            }
+
             var gmlManager = context.RequestServices.GetRequiredService<IGmlManager>();
             await endpoint.Execute(context, gmlManager, context.RequestServices);
             return;
