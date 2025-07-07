8612c7d110737bfcc0b44ada9e1a7bc35952a233
Terentev A. A. <terentev.a.a@recloud.tech>
2025-07-07 11:01:30 +0300

    Enable nullable reference types and update code to handle nullable values in `Textures` and `MinecraftHandler`

--- src/Gml.Web.Api.Dto/Minecraft/AuthLib/Textures.cs
+++ src/Gml.Web.Api.Dto/Minecraft/AuthLib/Textures.cs
@@ -6,5 +6,5 @@ public class Textures
 {
     [JsonProperty("SKIN", NullValueHandling = NullValueHandling.Ignore)] public SkinCape Skin { get; set; }
 
-    [JsonProperty("CAPE", NullValueHandling = NullValueHandling.Ignore)] public SkinCape Cape { get; set; }
+    [JsonProperty("CAPE", NullValueHandling = NullValueHandling.Ignore)] public SkinCape? Cape { get; set; }
 }
