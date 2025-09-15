a830e82e721ff961e3d3a47d224c113d30e2aa87
Terentev A. A. <terentev.a.a@recloud.tech>
2025-09-15 08:13:42 +0300

    Add `IsSystem` property to `PermissionDto` to indicate system-defined permissions

--- src/Gml.Web.Api.Dto/Auth/PermissionDto.cs
+++ src/Gml.Web.Api.Dto/Auth/PermissionDto.cs
@@ -2,5 +2,6 @@ public class PermissionDto
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public bool IsSystem { get; set; }
     public string? Description { get; set; }
 }
